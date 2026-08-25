c655e16477c28d0836a329f23d37cac1ec9b6aee
Bazyl Rei <bazylrei@gmail.com>
2026-08-25 16:18:56 +0100

    pushing to main

--- src/App.tsx
+++ src/App.tsx
@@ -3,5 +3,6 @@ export default function App() {
     <p style={{ color: "#1a1a1a", fontSize: "1.25rem", letterSpacing: "0.08em", fontWeight: 300 }}>
       A beginning of a new meme.
     </p>
+
   );
 }
